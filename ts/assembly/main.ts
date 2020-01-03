a0f0f0fda9e59b858c0690cf891a6c602a189441
rain <493123129@qq.com>
2020-01-03 16:04:26 +0800

    front src -> ts src

--- ts/assembly/main.ts
+++ ts/assembly/main.ts
@@ -1,22 +1,51 @@
-import { logging, context } from "near-runtime-ts";
-// available class: near, context, storage, logging, base58, base64, 
+import { logging, storage, math } from "near-runtime-ts";
+// available class: near, context, storage, logging, base58, base64,
 // PersistentMap, PersistentVector, PersistentDeque, PersistentTopN, ContractPromise, math
 import { TextMessage } from "./model";
+import { TemplateModel } from "./model";
 
-const NAME = ". Welcome to NEAR Protocol chain"
+const NAME = ". Welcome to NEAR Protocol chain";
 export function welcome(name: string): TextMessage {
-  logging.log("simple welcome test");
-  let message = new TextMessage()
-  const s = printString(NAME);
-  message.text = "Welcome, " + name + s;
-  logging.log(message.text);
-  return message;
+    logging.log("simple welcome test");
+    let message = new TextMessage();
+    const s = printString(NAME);
+    message.text = "Welcome, " + name + s;
+    logging.log(message.text);
+    return message;
 }
 
 export function hello(): string {
-  return "Hello, World!";
+    return "Hello, World!";
 }
 
 function printString(s: string): string {
-  return s;
+    return s;
+}
+
+/**
+ * 创建模板
+ * @param accountId 模板创建者
+ * @param templateName 模板名称
+ * @param templateContent 模板内容
+ * @param duration
+ */
+export function createTemplate(
+    accountId: string,
+    templateName: string,
+    templateContent: string,
+    duration: string = "0"
+): boolean {
+	let template = new TemplateModel();
+	template.id = math.hash32(math.randomSeed()); // TODO 此处应该有随机生成编号
+    template.name = templateName;
+    template.content = templateContent;
+    template.duration = duration;
+    storage.setBytes(accountId, template.encode());
+	
+    return true;
+}
+
+export function updateTemplate(accountId: string): string {
+	return "";
+	// return TemplateModel.decode(storage.getBytes(accountId));
 }
--- ts/assembly/model.ts
+++ ts/assembly/model.ts
@@ -3,3 +3,13 @@
 export class TextMessage {
     text: string;
 }
+
+/**
+ * 模板model
+ */
+export class TemplateModel {
+    public id: u32;
+    public name: string;
+    public content: string;
+    public duration: string;
+}
