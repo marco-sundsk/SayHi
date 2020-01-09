86b92529a12161406afa4edffb6b6c7d312d2da0
rain <493123129@qq.com>
2020-01-09 12:20:00 +0800

    接口改动

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,102 +2,21 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use near_bindgen::collections::Map;
 use near_bindgen::{env, near_bindgen};
 use std::collections::HashMap;
+pub mod model;
+
+// 1、创建模板
+// 2、创建卡片 包含卡片标题、私密信息、公开信息、红包等
+//      不定向发卡（接收到二维码的人扫描，仅能扫描一次）
+//      指定人发卡
+// 3、扫描卡片并创建联系人
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // 模板model
-#[derive(Clone, Default, BorshDeserialize, BorshSerialize)]
-pub struct Template {
-    pub id: String,         // 模板唯一编号
-    pub name: String,       // 模板名称
-    pub current_block: u64, // 模板创建时块高
-    pub duration: u64,      // 模板超时块数
-}
-
-impl Template {
-    pub fn new(id: String, new_name: String, new_current_block: u64, new_duration: u64) -> Self {
-        Template {
-            id: id,
-            name: new_name,
-            current_block: new_current_block,
-            duration: new_duration,
-        }
-    }
-}
-
-// 名片model
-#[derive(Clone, Default, BorshDeserialize, BorshSerialize)]
-pub struct Card {
-    pub id: String,              // 名片唯一编号
-    pub template_id: String,     // 模板唯一编号
-    pub card_type: u8,           // 卡片类型0为不定向多人，1为指定某人
-    pub public_message: String,  // 公开消息
-    pub private_message: String, // 私密消息
-    pub name: String,            // 名片名称
-    pub count: u64,              // 名片数量
-    pub remaining_count: u64,    // 剩余名片数量
-    pub is_avg: bool,            // 是否均分
-    pub total: u64,              // 总红包
-    pub remaining_total: u64,    // 剩余总红包
-    pub current_block: u64,      // 名片创建时块高
-    pub duration: u64,           // 名片超时块数
-    pub specify_account: String, // 指定接收人，当card_type为1时必填
-}
-
-impl Card {
-    pub fn new(
-        id: String,
-        template_id: String,
-        card_type: u8,
-        public_message: String,
-        private_message: String,
-        new_name: String,
-        new_count: u64,
-        is_avg: bool,
-        new_total: u64,
-        new_current_block: u64,
-        new_duration: u64,
-        new_specify_account: String,
-    ) -> Self {
-        Card {
-            id,
-            template_id,
-            card_type: card_type,
-            public_message: public_message,
-            private_message: private_message,
-            name: new_name,
-            count: new_count,
-            remaining_count: new_count,
-            is_avg: is_avg,
-            total: new_total,
-            remaining_total: new_total,
-            current_block: new_current_block,
-            duration: new_duration,
-            specify_account: new_specify_account,
-        }
-    }
-}
-
-// 联系人(收到的卡片以及被接收者查看后创建的联系人)model
-#[derive(Clone, Default, BorshDeserialize, BorshSerialize)]
-pub struct ContactPerson {
-    pub id: String,             // 联系人唯一编号
-    pub contact_person: String, //联系人姓名
-    pub card_count: u64,        // 收到的卡片数量
-    pub duration: u64,          // 名片超时块数
-}
-
-impl ContactPerson {
-    pub fn new(id: String, new_contact_person: String, new_card_count: u64, duration: u64) -> Self {
-        ContactPerson {
-            id: id,
-            contact_person: new_contact_person,
-            card_count: new_card_count,
-            duration: duration,
-        }
-    }
-}
+type Template = model::Template;
+type Card = model::Card;
+type ContactPerson = model::ContactPerson;
 
 // 用于提供访问服务
 #[near_bindgen]
@@ -106,9 +25,9 @@ pub struct BLCardService {
     template_record: Map<String, Vec<Template>>, // key 为账号信息，value 为模板列表
     template_account_relation: Map<String, String>, // key 为模板唯一编号，value 为账号信息，用于反向查找
 
-    card_record: Map<String, Vec<Card>>, // key 为账号信息，value 为名片列表
+    card_record: Map<String, Map<String, Card>>, // 外层 key 为账号信息，value 为名片信息，内层 key 为cardId，value 为card
     card_account_relation: Map<String, String>, // key 为名片唯一编号，value 为账号信息，用于反向查找
-    card_scan_result: Map<String, u8>,          // key 为名片唯一编号，value 为是否
+    card_scan_result: Map<String, bool>,        // key 为  名片_扫描人 唯一编号，value 为是否
 
     contract_person: Map<String, Vec<ContactPerson>>, // key 为账号信息，value 为联系人列表
 }
@@ -169,38 +88,40 @@ impl BLCardService {
         duration: u64,
         specify_account: String,
     ) -> bool {
-        if card_type == 0 || card_type == 1 {
-            // 根据card_type判断为指定给某人的card或不定向
-            // 给指定人发card，是否需要知道扫没扫描
-            // 给指定人发card前，发送人联系人列表里有接收人，接收人列表里有发送人嘛，还是一定要等接收人扫了才会有
-            let account_id = env::signer_account_id();
-            let mut cards: Vec<Card> = Vec::new();
-            if let Some(list) = self.card_record.get(&account_id) {
-                cards = list.to_vec();
-            }
-            let current_block_index = env::block_index();
-            // 创建卡片
-            let new_card = Card::new(
-                name.to_string(),
-                template_id,
-                card_type,
-                public_message,
-                private_message,
-                name.to_string(),
-                count,
-                true,
-                total,
-                current_block_index,
-                duration,
-                specify_account,
-            ); // TODO 第一个参数应该为card id
-            cards.push(new_card);
-            self.card_record.insert(&account_id, &cards);
-            self.card_account_relation.insert(&account_id, &account_id); // TODO 第二个参数应该为card id
-            return true;
-        } else {
+        if card_type != 0 && card_type != 1 {
             return false;
         }
+        // 根据card_type判断为指定给某人的card或不定向
+        // 给指定人发card，是否需要知道扫没扫描
+        // 给指定人发card前，发送人联系人列表里有接收人，接收人列表里有发送人嘛，还是一定要等接收人扫了才会有
+        let account_id = env::signer_account_id();
+        let mut cards: Map<String, Card> = Map::default();
+
+        if let Some(map) = self.card_record.get(&account_id) {
+            cards = map;
+        }
+
+        let current_block_index = env::block_index();
+        // 创建卡片
+        let new_card = Card::new(
+            name.to_string(),
+            template_id,
+            card_type,
+            public_message,
+            private_message,
+            name.to_string(),
+            count,
+            true,
+            total,
+            current_block_index,
+            duration,
+            specify_account,
+        ); // TODO 第一个参数应该为card id
+
+        cards.insert(&name, &new_card); // TODO 第一个参数应该为card id
+        self.card_record.insert(&account_id, &cards);
+        self.card_account_relation.insert(&name, &account_id); // TODO 第二个参数应该为card id
+        return true;
     }
 
     // 列出指定账号的名片信息
@@ -208,7 +129,8 @@ impl BLCardService {
         self.card_record.get(&account_id).map(|record| {
             let mut temp: Vec<HashMap<String, String>> = Vec::new();
 
-            for item in record.iter() {
+            for key in record.keys() {
+                let item = record.get(&key).unwrap();
                 let mut temp_map: HashMap<String, String> = HashMap::new();
                 temp_map.insert(String::from("id"), item.id.to_string());
                 temp_map.insert(String::from("template_id"), item.template_id.to_string());
@@ -242,7 +164,38 @@ impl BLCardService {
     }
 
     // 扫卡人创建联系人
-    pub fn create_contract_person(&mut self, contact_person: String, duration: u64) -> bool {
+    pub fn create_contract_person(
+        &mut self,
+        contact_person: String,
+        card_id: String,
+        duration: u64,) -> bool {
+        // 1、判断卡是否存在；2、判断是否是不定向卡片
+        let car_obj = self.card_account_relation.get(&card_id); // car_obj 值为创建人
+
+        if let None = car_obj {
+            env::log("卡片不存在".as_bytes());
+            return false;
+        }
+
+        // card_vec 为 car_obj 创建的所有卡片列表，在此卡片列表中查找卡片
+        let card_map = self.card_record.get(&car_obj.unwrap()).unwrap();
+        let item = card_map.get(&card_id).unwrap();
+        if item.id == card_id {
+            if item.card_type == 1 && item.remaining_count == 0 {
+                // 不定向
+                return false;
+            }
+        }
+
+        let mut scan_result_key = String::from(&card_id);
+        scan_result_key.push_str("_");
+        scan_result_key.push_str(&contact_person);
+
+        // 此处用于判断是否已经扫描过
+        if let Some(_) = self.card_scan_result.get(&scan_result_key) {
+            return false;
+        }
+
         let account_id = env::signer_account_id();
         let mut contact_person_vec: Vec<ContactPerson> = Vec::new();
 
@@ -262,16 +215,18 @@ impl BLCardService {
             temp_count = temp_count + 1;
         }
         contact_person_vec.remove(temp_count);
-        
         let new_contract_person = ContactPerson::new(
             contact_person.to_string(),
             contact_person.to_string(),
             old_card_count as u64 + 1,
             duration,
         ); // TODO 第一个参数应该生成；第三个参数应该去查询已经收到的数量
+
         contact_person_vec.push(new_contract_person);
         self.contract_person
             .insert(&account_id, &contact_person_vec);
+        self.card_scan_result.insert(&scan_result_key, &true);
+
         return true;
     }
 
@@ -296,7 +251,6 @@ impl BLCardService {
             temp_count = temp_count + 1;
         }
         contact_person_vec.remove(temp_count);
-        
         let new_contract_person = ContactPerson::new(
             account_id.to_string(),
             account_id.to_string(),
@@ -304,11 +258,11 @@ impl BLCardService {
             duration,
         ); // TODO 第一个参数应该生成；第三个参数应该去查询已经收到的数量
         contact_person_vec.push(new_contract_person);
-        self.contract_person
-            .insert(&sender, &contact_person_vec);
+        self.contract_person.insert(&sender, &contact_person_vec);
         return true;
     }
 
+    // 获取联系人列表
     pub fn list_contract_person(&self, account_id: String) -> Option<Vec<HashMap<String, String>>> {
         self.contract_person.get(&account_id).map(|record| {
             let mut temp: Vec<HashMap<String, String>> = Vec::new();
@@ -399,5 +353,3 @@ mod tests {
         // }
     }
 }
-
-// 创建联系人缺少为发卡人创建过程；创建卡片暂时不存在为指定人创建；扫描卡片未实现（扫描后数量减少或记录）；
